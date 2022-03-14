707454b68d17b9c04127057f26409f4dcee1160f
ozpv <39195175+ozpv@users.noreply.github.com>
2022-03-14 18:45:29 -0500

    Update blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "$HOME/.config/dwm/network.sh", 1, 0},
-    {"🌡 ", "$HOME/.config/dwm/weather.sh", 10000, 0},
+    	{"🌡 ", "$HOME/.config/dwm/weather.sh", 10000, 0},
 	{"", "$HOME/.config/dwm/volume.sh", 1, 0},
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0},
 	{" ", "date '+%a %b %d %I:%M %p'", 5, 0},
