9597d03f9ca07ce91edc1e05ab57d446fcbba04d
ozpv <39195175+ozpv@users.noreply.github.com>
2022-05-15 13:30:49 -0500

    update glyphs

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,3 +1,3 @@
 #status="$(cat /sys/class/net/w*/operstate 2>/dev/null)"
 status="$(cat /sys/class/net/e*/operstate 2>/dev/null)"
-[ "$status" == "up" ] && echo "直 online" || echo "睊 offline"  
+[ "$status" == "up" ] && echo " 直 online" || echo  "睊 offline"  
